59dd78c46dfd6ad67c48e60b810eb49af101fc55
Sergey Bespalov <sergei.bespalov@gmail.com>
2015-10-18 22:43:34 +0300

    ch.6 done

--- GeoQuiz/app/src/main/java/com/s99/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/s99/geoquiz/CheatActivity.java
@@ -1,12 +1,16 @@
 package com.s99.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,6 +25,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private Button mShowAnswer;
     private TextView mAnswerTextView;
+    private TextView mApiTextView;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue){
         Intent i = new Intent(packageContext, CheatActivity.class);
@@ -51,10 +56,34 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
         mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        mApiTextView = (TextView) findViewById(R.id.apiLevelTextView);
+        String api = "API level " + String.valueOf(Build.VERSION.SDK_INT);
+        mApiTextView.setText(api);
     }
 
     private void setAnswerShownResult(boolean isAnswerShown){
